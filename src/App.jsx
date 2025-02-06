a2e04390a08b2009066d399b3aecc21db1112313
RemezovMax <remezovmaxx@gmail.com>
2025-02-06 21:36:03 +0400

    GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Conditions from './pages/Conditions';
 import Calculation from './pages/Calculation';
