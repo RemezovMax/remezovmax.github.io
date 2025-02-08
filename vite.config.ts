f192df8e7dad5b68f0a5e2105f9a14d3ced07ebd
RemezovMax <remezovmaxx@gmail.com>
2025-02-08 19:12:50 +0400

    Move build files to root for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Указывается корневой путь
+  base: "/https://remezovmax.github.io/", // Указывается корневой путь
   server: {
     port: 3000,
   },
