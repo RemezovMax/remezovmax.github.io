d885bb6928f27d20b7b8aa8793a9076799869a2f
RemezovMax <remezovmaxx@gmail.com>
2025-02-08 22:23:23 +0400

    Migrate to main for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Определяем базовый путь в зависимости от среды
-const basePath = process.env.NODE_ENV === "production" ? "/remezovmax.github.io/" : "/";
-
 export default defineConfig({
   plugins: [react()],
-  base: basePath, // Динамически устанавливаем base
+  base: "/", // Указываем корень для GitHub Pages
   server: {
-    port: 3000, // Порт для локальной разработки
+    port: 3001, // Порт для локальной разработки http://localhost/
   },
 });
